d34539ff322501d7e0e94f282ceb6c774fe5c000
RawsonAlom <mdhridoykhanbd029@gmail.com>
2025-02-08 23:51:49 +0600

    My Website 2.o update

--- test.js
+++ test.js
@@ -25,6 +25,27 @@ let letter = "";
 		setTimeout(type, 100);
 	}
 })();
+
+// Troggle effect Controler
+
+function Fwellcome() {
+	document.getElementById("Rawson-home").style = "display:none;";
+	document.getElementById("Wellcome-txt").style = "display:none;";
+	document.getElementById("Rawson-txt").style =
+		"display:flex;background-color: rgb(0, 255, 255);width: 20%;height: 582px;border-top-right-radius: 100px;border-bottom-right-radius: 100px;justify-content: center;align-items: center;";
+	document.getElementById("Wellcome-home").style =
+		"display: flex;width: 80%;height: 582px;border-top-right-radius: 10px;border-bottom-right-radius: 10px;border-top-left-radius: 150px;border-bottom-left-radius: 150px;align-items: center;justify-content: center;flex-direction: column;background-color: rgb(234, 234, 234);";
+}
+
+function Frawson() {
+	document.getElementById("Rawson-home").style =
+		"display:flex;width: 80%;height: 590px;border-top-right-radius: 10px;border-bottom-right-radius: 10px;border-top-left-radius: 300px;align-items: center;justify-content: center;flex-direction: column;";
+	document.getElementById("Wellcome-txt").style =
+		"display:flex;background-color: aqua;width: 20%;height: 582px;border-top-left-radius: 100px;border-bottom-left-radius: 100px;justify-content: center;align-items: center;";
+	document.getElementById("Rawson-txt").style = "display:none;";
+	document.getElementById("Wellcome-home").style = "display:none;";
+}
+
 /*
 // Home Page Hover Controler
 
